6bbc28fb38c3ce5fa3192001d34c0c9228250879
Bryce Thomas <bryce.m.thomas@gmail.com>
2020-08-21 20:11:25 -0700

    Add Zenburn theme.

--- init.el
+++ init.el
@@ -1,4 +1,18 @@
 ;; misc
+
+;; based off instructions at https://melpa.org/#/getting-started
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (setq-default fill-column 80) 
 (setq column-number-mode t)         ;; show column number
 (setq-default indent-tabs-mode nil) ;; use spaces not tabs
@@ -24,3 +38,20 @@
 ;; move between buffers with shift + arrows instead of C-x o
 (when (fboundp 'windmove-default-keybindings)
   (windmove-default-keybindings))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (zenburn-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; For new machines, run these commands once:
+;; M-x package-refresh-contents
+;; M-X package-install zenburn-theme
+(load-theme 'zenburn t)
